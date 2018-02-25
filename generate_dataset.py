0bf21d8f6a9f13b3413c168809c4dc2ddb9e1d1a
Dirk <dirkmeulenbelt@gmail.com>
2018-02-25 21:59:54 +0100

    first

---
+++ generate_dataset.py
@@ -0,0 +1,244 @@
+import MalmoPython
+import itertools
+import sys
+import os
+import time
+import random
+from object_constructors import *
+from PIL import Image
+
+def get_settings():
+	which_object = ""
+	while which_object != 'tree' and which_object != 'house' and which_object != 'art' and which_object != 'mansion':
+		print()
+		print("Set the correct object in the experimental world.")
+		print("-------------------------------------------------")
+		which_object = input("Which object do you wish to take images of? Choose from: 'tree', 'house', 'mansion', or 'art'  ")
+
+	x_dimension = 0
+	while not x_dimension:
+		print()
+		print("Set the X pixelsize")
+		print("-------------------------------------------------")
+		x_dimension = int(input("What is the pixelsize of your image in the X axis?  "))
+
+	y_dimension = 0
+	while not y_dimension:
+		print()
+		print("Set the Y pixelsize")
+		print("-------------------------------------------------")
+		y_dimension = int(input("What is the pixelsize of your image in the Y axis?  "))
+
+	return which_object, x_dimension, y_dimension
+
+#which_object,x_dimension,y_dimension = get_settings()
+#Settings:
+which_object = 'mansion'
+x_dimension = 640
+y_dimension = 480
+d1 = 6
+d2 = 9
+d3 = 12
+
+def add_object(which_object, x = 0, z = 0):
+	if which_object == "tree":
+		return gen_tree(x,z)
+
+	elif which_object == "house":
+		return gen_house(x,z, True)
+
+	elif which_object == 'mansion':
+		return gen_house(x,z, False)
+
+	elif which_object == "art":	
+		return gen_art_sphere(x,z)
+
+def generate_positions(d1,d2,d3):
+	coordinates = [
+	(0,d1,180),
+	(0,d2,180),
+	(0,d3,180),
+	(-d1,d1,-130),
+	(-d2,d2,-130),
+	(-d3,d3,-130),
+	(-d1,0,-85),
+	(-d2,0,-85),
+	(-d3,0,-85),
+	(-d1,-d1,-45),
+	(-d2,-d2,-45),
+	(-d3,-d3,-45),
+	(0,-d1,-5),
+	(0,-d2,-5),
+	(0,-d3,-5),
+	(d1,-d1,40),
+	(d2,-d2,40),
+	(d3,-d3,40),
+	(d1,0,85),
+	(d2,0,85),
+	(d3,0,85),
+	(d1,d1,135),
+	(d2,d2,135),
+	(d3,d3,135),
+	]
+
+	print("Created " + str(len(coordinates)) + " vantage points.")
+	return coordinates
+
+def generate_focus_points():
+	focus_points = []
+	yaws = [-30,0,30]
+	pitches = [10,0,-25]
+	for i in yaws:
+		for j in pitches:
+			focus_points.append((i,j))
+	return focus_points
+
+
+mission_XML = '''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
+		<Mission xmlns="http://ProjectMalmo.microsoft.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">        
+				
+			<About>
+				<Summary>Costa del Sol</Summary>
+			</About> 
+			
+			<ServerSection>
+			   
+				<ServerInitialConditions>
+					<Time>
+						<StartTime>12000</StartTime>
+						<AllowPassageOfTime>false</AllowPassageOfTime>
+					</Time>
+					<Weather>clear</Weather>
+				</ServerInitialConditions>
+				
+				<ServerHandlers>
+					<!-- removed ';decoration' at the end of the generatorstring below -->
+					<FlatWorldGenerator generatorString="3;7,2*3,2;1"/>
+		
+					<!-- <ServerQuitFromTimeUp timeLimitMs="30000"/> -->
+		
+					<DrawingDecorator>
+						
+							<!-- Add object -->
+							''' + add_object(which_object) + '''
+
+					</DrawingDecorator>          
+				</ServerHandlers>
+			</ServerSection>
+									
+			<AgentSection mode="Survival">
+					
+				<Name>Steve</Name>
+									
+				<AgentStart>
+				
+					<Placement x="5" y="4" z="5" yaw="0"/>
+				
+					<Inventory>
+						<InventoryItem slot="0" type="diamond_sword"/>
+					</Inventory>
+				
+				</AgentStart>
+				
+				<AgentHandlers>
+
+					<MissionQuitCommands/>
+					
+					<ObservationFromFullStats/>
+					
+					<ObservationFromRay/>
+					
+					<AbsoluteMovementCommands />
+				
+				</AgentHandlers>
+				
+			</AgentSection>
+							
+		</Mission>'''
+
+#This sets up the API objects.
+agent_host = MalmoPython.AgentHost()
+try:
+	agent_host.parse( sys.argv )
+except RuntimeError as e:
+	print('ERROR:',e)
+	print(agent_host.getUsage())
+	exit(1)
+if agent_host.receivedArgument("help"):
+	print(agent_host.getUsage())
+	exit(0)
+
+#Build a mission and give it the configuration of the world. Note that we can change that later with Python code.
+my_mission = MalmoPython.MissionSpec(mission_XML, True)
+my_mission_record = MalmoPython.MissionRecordSpec()
+
+agent_host.setVideoPolicy(MalmoPython.VideoPolicy.LATEST_FRAME_ONLY)
+my_mission.requestVideo(x_dimension,y_dimension)
+my_mission.forceWorldReset()
+
+# Attempt to start a mission:
+max_retries = 3
+for retry in range(max_retries):
+    try:
+        agent_host.startMission( my_mission, my_mission_record )
+        break
+    except RuntimeError as e:
+        if retry == max_retries - 1:
+            print("Error starting mission:",e)
+            exit(1)
+        else:
+            time.sleep(2)
+
+# Loop until mission starts:
+print("Waiting for the mission to start ",)
+world_state = agent_host.getWorldState()
+while not world_state.has_mission_begun:
+    sys.stdout.write(".")
+    time.sleep(0.1)
+    world_state = agent_host.getWorldState()
+    for error in world_state.errors:
+        print("Error:",error.text)
+
+positions = generate_positions(4,8,10)
+focus_points = generate_focus_points()
+print(focus_points)
+
+shot = 230
+for pos in positions:
+	
+	x,z,yaw = pos
+	agent_host.sendCommand('tp {0} 4 {1}'.format(x,z))
+	agent_host.sendCommand('setYaw {0}'.format(yaw))
+	#agent should now face the tree.
+
+	for focus in focus_points:
+		yaw2, pitch = focus 
+		agent_host.sendCommand('setYaw {0}'.format(yaw+yaw2))
+		agent_host.sendCommand('setPitch {0}'.format(pitch))
+		
+		world_state = agent_host.peekWorldState()
+		while world_state.is_mission_running and all (e.text=='{}' for e in world_state.observations):
+			world_state = agent_host.peekWorldState()
+		
+		assert len(world_state.video_frames) > 0, 'No video frames!? We just checked for them.'
+		frame = world_state.video_frames[-1]
+		image = Image.frombytes('RGB', (frame.width, frame.height), bytes(frame.pixels) )		
+		
+		#The below line saves the images to the given path.
+		image.save("Snapshots/{0}s/{0}{1}.jpg".format(which_object,shot))
+		shot += 1
+
+print("Done!")
+agent_host.sendCommand("quit 1")
+
+# Loop until mission ends:
+while world_state.is_mission_running:
+    sys.stdout.write(".")
+    time.sleep(0.1)
+    world_state = agent_host.getWorldState()
+    for error in world_state.errors:
+        print("Error:",error.text)
+
+print()
+print("Mission ended")
+# Mission has ended.
---
+++ object_constructors.py
@@ -0,0 +1,109 @@
+import random
+
+def draw_cuboid(x1, x2, z1, z2, y1, y2 ,t ):
+	return '<DrawCuboid x1="{x1}" x2="{x2}" z1="{z1}" z2="{z2}" y1="{y1}" y2="{y2}" type="{t}"/>'.format(
+	  x1= x1,
+	  x2= x2,
+	  z1= z1,
+	  z2= z2,
+	  y1= y1,
+	  y2= y2,
+	  t=t)
+
+def gen_tree(x,z):
+
+	tree_length = random.randint(3,6)
+		
+	start = float(random.randint(1,3))
+	tree_foliage_start = int( round( (start / (start+1)) * float(tree_length) ) )
+	leaves = random.choice(['leaves','leaves2'])
+	log = random.choice(['log','log2'])
+
+	#Make top foliage
+	generatorString = '<DrawBlock x="{0}" z="{1}" y="{2}" type="{3}"/>'.format(x, z, tree_length+6, leaves)
+	generatorString += draw_cuboid(x-1,x+2,z-1,z+1,tree_length+5,tree_length+5,leaves)
+	
+	foliage_around_trunk = tree_length - tree_foliage_start + 1
+	for i in range(foliage_around_trunk):
+		#max reach
+		s = 1 #Can change this to create wider foliage.
+		generatorString += draw_cuboid(x-s-i,x+s+i,z-s-i,z+s+i,tree_length-i+4,tree_length-i+4,leaves)
+
+	#I'm putting this here because I think later entries override earlier ones, and I want a stem instead of foliage.
+	generatorString += draw_cuboid(x,x,z,z,4,tree_length+4,log)
+	return generatorString
+
+def gen_house(x, z, small=False):
+	
+	generatorString= ''
+
+	#There is no logic to the choices. Just picked a few.
+	door = random.choice(['acacia_door','jungle_door', 'spruce_door', 'dark_oak_door'])
+	wall = random.choice(['glass','ice','cobblestone','mossy_cobblestone','brick_block','stonebrick']) 
+	roof = random.choice(['glass','brick_block', 'clay'])
+	
+	if small:
+		w = random.randint(1,3)
+		l = random.randint(2,4)
+		h = random.randint(2,3)
+	else:
+		w = random.randint(4,10)
+		l = random.randint(5,10)
+		h = random.randint(3,7)
+
+	house_has_fence = random.choice([True,False])
+	if house_has_fence:
+		fence = random.choice(['spruce_fence', 'jungle_fence', 'birch_fence', 'dark_oak_fence' ])
+		fence_gate = '{0}_{1}'.format(fence,'gate')
+		d = random.randint(2,3)
+		#Front fence, back fence, left fence right fence
+		generatorString += draw_cuboid(x-w-d, x+w+d, z+d,   z+d,   4,4, fence)
+		generatorString += draw_cuboid(x-w-d, x+w+d, z-l-d, z-l-d, 4,4, fence)
+		generatorString += draw_cuboid(x-w-d, x-w-d, z+d,   z-l-d, 4,4, fence)
+		generatorString += draw_cuboid(x+w+d, x+w+d, z+d,   z-l-d, 4,4, fence)
+		#Add fence door
+		generatorString += '<DrawBlock x="{0}" z="{1}" y="4" type="{2}"/>'.format(x,z+d,fence_gate)
+	#Draw walls
+	#Front wall, back wall, side wall left, side wall right:
+	generatorString += draw_cuboid(x-w, x+w, z,   z,   4, h+4, wall)
+	generatorString += draw_cuboid(x-w, x+w, z-l, z-l, 4, h+4, wall) 
+	generatorString += draw_cuboid(x-w, x-w, z,   z-l, 4, h+4, wall)
+	generatorString += draw_cuboid(x+w, x+w, z,   z-l, 4, h+4, wall)
+
+	#Add some sort of roof 
+	if small:
+		r = 2
+	else:
+		r = 5
+	for i in range(r):
+		generatorString += draw_cuboid(x+w-i,x-w+i,z,z-l+i,h+4+i,h+4+i,roof)
+	#Draw door at the x,z
+	generatorString += '<DrawBlock x="{0}" z="{1}" y="4" type="{2}"/>'.format(x,z,door)
+	generatorString += '<DrawBlock x="{0}" z="{1}" y="5" type="{2}"/>'.format(x,z,door)
+
+	#Draw windows on the side opposite to the roof
+	if wall != 'glass': 
+		generatorString += '<DrawBlock x="{0}" z="{1}" y="5" type="{2}"/>'.format(x+1,z-l,'glass')
+		generatorString += '<DrawBlock x="{0}" z="{1}" y="5" type="{2}"/>'.format(x-1,z-l,'glass')
+
+	#Add some NPCs
+	people = [random.choice(['Cow','Chicken','Villager','Sheep']) for i in range( random.randint(2,4) )]
+	for i in people:
+		generatorString += '<DrawEntity x="{0}" z="{1}" y="{2}" type="{3}"/>'.format(x-2,z+4,4,i)
+	return generatorString
+
+def gen_sapling(x,y):
+	return '<DrawBlock x="{0}" z="{1}" y="{2}" type="{3}"/>'.format(x,y,4,'sapling')
+
+def gen_art_sphere(x,z):
+	w = random.randint(1,3)
+	h = random.randint(1,3)
+	o = ['obsidian', 'ice', 'wool', 'diamond_block']
+	stuff = random.choice(o)
+	stuff2 = random.choice(o)
+	generatorString = draw_cuboid(x-w,x+w,z-w,z+w,4,4+h,stuff)
+	generatorString += '<DrawSphere x="{0}" z="{1}" y="{2}" radius="{3}" type="{4}" />'.format(x,z,10+h,w*2,stuff2)
+	return generatorString
+
+def gen_something_that_is_not_a_tree_but_is_similar_for_foolings():
+	pass
